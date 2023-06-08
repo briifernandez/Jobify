e490b4c4fa3bf6cef44eb3d6d5091cbf0217990e
briifernandez <bfernandez@ucsd.edu>
2023-06-07 18:47:53 -0700

    Server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Server running...')
+import express from 'express'
+const app = express()
+
+
+const port = process.env.PORT || 5000
+
+
+
+app.listen(port, ()=> {
+    console.log(`Server is listening on port ${port}... `)
+})
+
