cc53ff0daa3f3a75609f07f990fbc90351379222
briifernandez <bfernandez@ucsd.edu>
2023-06-07 21:43:45 -0700

    Controllers,DB,Middleware,Routes,Server

--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 import express from 'express'
 const app = express()
+import dotenv from 'dotenv'
+dotenv.config()
 
+//db and autheticateUser
+import connectDB from './db/connect.js'
 
-const port = process.env.PORT || 5000
+//routers 
+import authRouter from './routes/authRoutes.js'
 
+//middleware
 
+import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
 
-app.listen(port, ()=> {
-    console.log(`Server is listening on port ${port}... `)
+//makes json available to the controllers
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    throw new Error('error')
+    res.send('Welcome!')
 })
 
+app.use('/api/v1/auth', authRouter)
+
+
+app.use(notFoundMiddleware) 
+app.use(errorHandlerMiddleware)
+const port = process.env.PORT || 3000 
+
+
+
+
+//only want to spin up server if connection was successful
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        console.log(`Server is listening on port ${port}... `)
+    } 
+    catch (error) {
+        console.log(error)
+
+    }
+}
+
+//invoke function
+start()
