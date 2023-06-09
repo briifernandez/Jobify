ad3b960239b07f8c9cf792240e9a9ddf07c3d334
briifernandez <bfernandez@ucsd.edu>
2023-06-09 01:50:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,11 @@ const port = process.env.PORT || 4001
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URL)
-        console.log(`Server is listening on port ${port}... `)
+        app.listen(port, ()=> {
+            console.log(`Server is listening on port ${port}... `)
+
+        });
+        
     } 
     catch (error) {
         console.log(error)
