569060dbe4545c8757285b11207ca914de984c6d
briifernandez <bfernandez@ucsd.edu>
2023-06-08 22:26:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware) 
 app.use(errorHandlerMiddleware)
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 4001
 
 
 
