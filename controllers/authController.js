c41f5858ea9e1d5cd37040985f02f819c0cb3f12
briifernandez <bfernandez@ucsd.edu>
2023-07-10 13:27:38 -0700

    Error Handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,16 @@
 import User from '../models/User.js'
+import { StatusCodes } from 'http-status-codes'
 
-const register = async (req, res, next) => {
-   
-        const user = await User.create(req.body)
-        res.status(201).json({user})
+const register = async (req, res) => {
+        const {name,email,password} = req.body
+
+        //if fields are empty
+        if(!name || !email || !password) {
+            throw new Error('please provide all values')
+        }
+
+        const user = await User.create({name, email, password})
+        res.status(StatusCodes.CREATED).json({user})
 
         //adding next to parameters and below is good for error-handling with hard coded logic and can figure out if the error is mongoose or user with meaningful errors in postman
     
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,40 @@
+import { StatusCodes } from 'http-status-codes'
+
+//1. it is able to handle our database errors -mongoose
+//2. there are also seperate controller errors
+
 const errorHandlerMiddleware = (err,req,res,next) => {
-    console.log(err);
-    res.status(500).json({msg: err})
+    //there is an error constructor with a message argument MDN docs
+    console.log(err.message);
+    const defaultError = {
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        //if err is present use it if not use generic
+        msg: err.message || 'Something went wrong, try again later',
+    }
+    //err represents the whole object in postman, not the errors object inside it
+
+    //ValidationError is the name property at the end, msg == err
+    if(err.name === 'ValidationError'){
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        // defaultError.msg = err.message //shows message from the bottom
+        defaultError.msg = Object.values(err.errors)
+        .map((item)=> item.message)
+        .join(',')
+
+    }
+
+    if(err.code && err.code === 11000) {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`
+
+    }
+    //shows whole err object with errors 
+    res.status(defaultError.statusCode).json({msg: err})
+
+    //concise message
+    // res.status(defaultError.statusCode).json({msg: defaultError.msg})
+
+    //email and password has to be unique
 } 
 
 export default errorHandlerMiddleware
